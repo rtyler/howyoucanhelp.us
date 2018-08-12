54c0ff733e615e2b62024aa913139785e13edcc0
R. Tyler Croy <tyler@monkeypox.org>
2018-08-12 08:52:03 -0700

    Bring back some frontend webapp stuff into the backend directory for serving
    It doesn't make sense right now to split the basic frontend application (simple
    introductory site) from the backend APIs at the moment.
    
    Since we're splitting on Mobile App / REST API, this application can hold onto
    everything for now.
    
    If we decide to build a full-featured mobile web app in the future, that should
    go back into frontend/

---
+++ backend/src/pwa/index.ts
@@ -0,0 +1,5 @@
+import feathers from '@feathersjs/feathers'
+
+const app : any = feathers();
+
+console.log(`Loaded feathers and ready to roll`);
---
+++ backend/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/pwa/index.ts',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+        options: {
+          useCache: true,
+        }
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      excludeChunks: [
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  }
+};
+
